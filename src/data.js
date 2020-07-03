ff6f08182a0fef13488d8d670c34119e4a598e64
Macarena Cuevas Guzmán <macarenacuevasg@gmail.com>
2020-07-03 12:49:36 -0400

    03-07-2020 intentando filtrar

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
+//función para filtrar por tag
+export const filterTag = (champions) =>{
+let arrayChampionstag=champions.filter(oli=>(oli.tags[0]=="Tank"||oli.tags[1]=="Tank"));
+return arrayChampionstag;
+};
 
-
-export const example = () => 'example';
-
-export const anotherExample = () => 'OMG';
+// export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,20 +1,68 @@
-//import { example } from './data.js';
+import { filterTag} from './data.js';
 import lol from './data/lol/lol.js';
 
 //Array que contiene los valores del data (en este caso cada uno de los champions)
 let champions=Object.values(lol.data);
 
-document.getElementById('allTheChamps').innerHTML = champions.map(champ => 
-      `<div class="containerEachChamp">
+//imprimir en pantalla los array de champions que escoja (todos o filtrados)
+let renderChampionsInScreen=(arrayOfChampions)=>{
+  document.getElementById('allTheChamps').innerHTML = arrayOfChampions.map(champ => 
+    `<div class="containerEachChamp">
       <p> ${champ.name.toUpperCase()}</p>
       <div id="containerProfilePic"> 
-        <img id="profilePic" src="${champ.splash}"> 
+        <img id="profilePic" src="${champ.splash}">
       </div>
       <p> "${champ.title}"</p>
       <p> [${champ.tags}]</p>
     </div>`
+  ).join('');
+};
 
-).join('');
+//imprimir todos 
+// renderChampionsInScreen(champions);
 
-// document.querySelector('div#allTheChamps > div').classList.add("EachChampContainer");
+
+let checkbox=document.getElementById("checkboxTags");
+let tagValue;
+
+let checked= () =>{
+  if(checkbox.checked=true)
+  tagValue=checkbox.value;
+  return tagValue
+}
+
+checkbox.addEventListener("click",checked);
+
+
+
+
+
+
+console.log(checkbox);
+
+// filterTag(champions);
+
+
+// document.getElementById("checkboxTags").addEventListener(click,validate)
+// function validate(){
+//   var selectChoose = document.getElementById('checkboxTags');
+//   var maxOptions = 2;
+//   var optionCount = 0;
+//   for (var i = 0; i < selectChoose.length; i++) {
+//     if (selectChoose[i].selected) {
+//       optionCount++;
+//       if (optionCount > maxOptions) {
+//         alert("validation failed, not submitting")
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
+
+
+//probando
+// console.log(filterTag(champions));
+
+
 
