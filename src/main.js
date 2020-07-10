b00f62886637b5dff6c78e594cb2751db52b4a85
Macarena Cuevas Guzmán <macarenacuevasg@gmail.com>
2020-07-10 15:46:34 -0400

    10-07-2020 modal

--- src/main.js
+++ src/main.js
@@ -12,10 +12,10 @@ const renderChampionsInScreen = (arrayOfChampions) => {
       <div id="containerProfilePic"> 
         <img id="profilePic" src="${champ.splash}">
       </div><br>
-      <p> [${champ.tags}]</p><br>
+      <p> [${champ.tags.join(' ')}]</p><br>
       <p><img id="attack" src="img/Melee_role.png"> ${champ.info.attack}&nbsp&nbsp<img id="defense" src="img/Armor_icon.png"> ${champ.info.defense}</p>
       <p><img id="magic" src="img/Mana_regeneration_icon.png">${champ.info.magic} &nbsp&nbsp<img id="difficulty" src="img/Champion_style_abilities_active.png">${champ.info.difficulty}</p>
-      <div class="containerModals">
+      <div class="containerModal">
       </div>
       </div>`).join('');
 };
@@ -26,8 +26,7 @@ renderChampionsInScreen(champions);
 // FILTER TAG
 // seleccion todos los filtros tag
 const checkbox = document.querySelector('#checkboxTags');
-
-// función que recoge nombre del tag escogido e imprime
+// función que recoge nombre del tag escogido y luego imprime con renderChampionsInScreen
 function valueFilterTag() {
   if (document.querySelector('#checkboxTags :checked') !== null) {
     const tagValue = document.querySelector('#checkboxTags :checked').value;
@@ -39,8 +38,7 @@ checkbox.addEventListener('change', valueFilterTag);
 // ORDER INFORMATION
 // seleccionar filtros de informacion
 const selectList = document.querySelector('#selectInformation');
-
-// funcion que recoge el input escodigo e imprime
+// funcion que recoge el input escogido y luego imprime con renderChampionsInScreen
 function valueOrderInformation() {
   if (selectList !== null) {
     const valueSelectInformation = selectList.value;
@@ -61,63 +59,57 @@ selectList.addEventListener('change', mixFilterTagInformation);
 
 // SEARCHER
 const inputSearch = document.getElementById('inputSearch');
-
-// funcion que recoge input de busqueda y va filtrando automaticamente
+// funcion que recoge input de busqueda y va filtrando automaticamente y luego imprime con renderChampionsInScreen
 function valueSearcher() {
   const inputValue = inputSearch.value.toUpperCase();
   renderChampionsInScreen(filterSearch(champions, inputValue));
 }
-
 inputSearch.addEventListener('keyup', valueSearcher);
 
-//parte del modal 
-
-const renderModalChampions = () => {
-  event.target.querySelector('.containerModals').style.display='block';
+// MODAL
+const showModalChampion = () => {
+  const actualModal = event.target.querySelector('.containerModal');
+  actualModal.style.display = 'block';
   for (let i = 0; i < champions.length; i++) {
-    if ((champions[i].name).toUpperCase() === event.target.firstElementChild.innerHTML){
-     
-  
-  event.target.querySelector('.containerModals').innerHTML=`<div class="modalEachChamp">
-  <span class="close">&times;</span>
-  <p> ${champions[i].name.toUpperCase()}</p><br>
-  <div id="containerModalPic"> 
-    <img id="modaPic" src="${champions[i].splash}">
-  </div><br>
-  <p> ${champions[i].title}</p><br>
-  <p>${champions[i].blurb}</p><br>
-  <div id="containerInfoModal">
-   <p>attack:${champions[i].info.attack}</p>
-   <p>defense:${champions[i].info.defense}</p>
-   <p>magic:${champions[i].info.magic}</p>
-   <p>difficulty:${champions[i].info.difficulty}</p>
-  </div>
-  <div id="containerStatsModal">
-   <p>hp:${champions[i].stats.hp}</p>
-   <p>mp:${champions[i].stats.mp}</p>
-   <p>movespeed:${champions[i].stats.movespeed}</p>
-   <p>armor:${champions[i].stats.armor}</p>
-   <p>attackrange:${champions[i].stats.attackrange}</p>
-   <p>hpregen:${champions[i].stats.hpregen}</p>
-   <p>mpregen:${champions[i].stats.mpregen}</p>
-   <p>attackdamage:${champions[i].stats.attackdamage}</p>
-  </div>
-  </div>`
-}
-
-}
-window.onclick = function(event) {
-  if (event.target == document.querySelector('.containerModals')){
-    document.querySelector('.containerModals').style.display = "none";
+    if ((champions[i].name).toUpperCase() === event.target.firstElementChild.innerHTML) {
+      // lo que contiene el contenedor containerModal
+      actualModal.innerHTML = 
+      `<div class="modalEachChamp">
+        <span class="close">&times;</span>
+      <p> ${champions[i].name.toUpperCase()}</p><br>
+      <div id="containerModalPic"> 
+        <img id="modaPic" src="${champions[i].splash}">
+      </div><br>
+      <p> ${champions[i].title}</p><br>
+      <p>${champions[i].blurb}</p><br>
+      <div id="containerInfoModal">
+        <p>attack:${champions[i].info.attack}</p>
+        <p>defense:${champions[i].info.defense}</p>
+        <p>magic:${champions[i].info.magic}</p>
+        <p>difficulty:${champions[i].info.difficulty}</p>
+      </div>
+      <div id="containerStatsModal">
+        <p>hp:${champions[i].stats.hp}</p>
+        <p>mp:${champions[i].stats.mp}</p>
+        <p>movespeed:${champions[i].stats.movespeed}</p>
+        <p>armor:${champions[i].stats.armor}</p>
+        <p>attackrange:${champions[i].stats.attackrange}</p>
+        <p>hpregen:${champions[i].stats.hpregen}</p>
+        <p>mpregen:${champions[i].stats.mpregen}</p>
+        <p>attackdamage:${champions[i].stats.attackdamage}</p>
+      </div>
+    </div>`;
+    }
   }
-}
+  // para cerrar el modal al hacer click afuera
+  window.onclick = (event) => {
+    if (event.target === actualModal) {
+      actualModal.style.display = 'none';
+    }
+  };
+  // para cerrar el modal al hacer click en el boton cerrar
+  event.target.querySelector('.close').onclick = () => {
+    actualModal.style.display = 'none';
+  };
 };
-
-
-
-document.querySelector("#allTheChamps").addEventListener('click', renderModalChampions);
-
-
-// document.querySelector('.close').addEventListener('click', () => {
-//   document.querySelector('.containerModals').style.display='none';
-// });
+document.querySelector('#allTheChamps').addEventListener('click', showModalChampion);
